c9880b037f0edf59d9c5441589df38a2cabb39c1
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-10-24 22:29:07 +0600

    fix: hero slider image not showing on production

--- src/components/Slider/data.js
+++ src/components/Slider/data.js
@@ -1,32 +1,32 @@
 export const heroSliderData = [
-    {
-      id: 1,
-      path: "../../../public/assets/01.jpg",
-      altText: "recipe photo",
-    },
-    {
-      id: 2,
-      path: "/assets/02.jpg",
-      altText: "recipe photo",
-    },
-    {
-      id: 3,
-      path: "/assets/03.png",
-      altText: "recipe photo",
-    },
-    {
-      id: 4,
-      path: "/assets/04.jpg",
-      altText: "recipe photo",
-    },
-    {
-      id: 5,
-      path: "/assets/05.png",
-      altText: "recipe photo",
-    },
-    {
-      id: 6,
-      path: "/assets/06.png",
-      altText: "recipe photo",
-    },
-  ];
+  {
+    id: 1,
+    path: "/assets/01.jpg",
+    altText: "recipe photo",
+  },
+  {
+    id: 2,
+    path: "/assets/02.jpg",
+    altText: "recipe photo",
+  },
+  {
+    id: 3,
+    path: "/assets/03.png",
+    altText: "recipe photo",
+  },
+  {
+    id: 4,
+    path: "/assets/04.jpg",
+    altText: "recipe photo",
+  },
+  {
+    id: 5,
+    path: "/assets/05.png",
+    altText: "recipe photo",
+  },
+  {
+    id: 6,
+    path: "/assets/06.png",
+    altText: "recipe photo",
+  },
+];
